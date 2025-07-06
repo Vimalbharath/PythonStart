5bea62dbc0d7bd24a08ebfb51e2a367a1761681a
Vimalbharath <vimalbharath21@gmail.com>
2025-07-06 08:45:23 +0530

    recursive func

---
+++ w3schools/trirec.py
@@ -0,0 +1,12 @@
+def tri_recursion(k):
+  print('hi')
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("Recursion Example Results:")
+tri_recursion(6)
+tri_recursion(-1)
