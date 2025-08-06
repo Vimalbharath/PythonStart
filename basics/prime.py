1d0f04461d77ad77c6c2ff0da0f591a01d4e8c67
Vimalbharath <vimalbharath21@gmail.com>
2025-08-06 20:34:44 +0530

    updated prime

--- basics/prime.py
+++ basics/prime.py
@@ -1,21 +1,28 @@
-num=int(input("Enter a number: "))
+# num=int(input("Enter a number: "))
 
 def prime1(num):
     temp=0
     for i in range(num):
         if(num %(i+1)==0):
-            temp=temp+14
+            temp=temp+1
             
 
     if(temp <=2):
         return True
     else:
         return False
+
+def prime2(num):
+    c=2
+    while(c*c<=num):
+        if(num%c==0):
+            return False
+        c=c+1
+    return True
     
 def main():
-    if(prime1(num)):
-        print("Prime Number")
-    else:
-        print("Not a prime")
+    for num in range(1,100):
+        if(prime1(num)):print(num)
+        if(prime2(num)):print(num)
 
 main()
