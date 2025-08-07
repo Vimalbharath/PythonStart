1988756d6c5c8792068db3070401477c796bc96c
Vimalbharath <vimalbharath21@gmail.com>
2025-08-07 20:56:25 +0530

    code corrected

--- binary/binarysearch.py
+++ binary/binarysearch.py
@@ -1,18 +1,18 @@
 def main():
     nums=[1,2,3,4,5,6,7]
-    element=9
+    element=7
     ans=binary(nums,element)
     print(ans)
 
 def binary(nums,element):
     if len(nums)==0: return -1
-    s,e=0,len(nums)
-    while e>s:
+    s,e=0,len(nums)-1
+    while s<=e:
         m=s+(e-s)//2
         if nums[m]>element:
-            e=m
+            e=m-1
         elif nums[m]<element:
-            s=m
+            s=m+1
         else:
             return m
     return -1
