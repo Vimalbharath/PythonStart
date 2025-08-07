c45ea0435d336eec6b50199d2de0796e079317ac
Vimalbharath <vimalbharath21@gmail.com>
2025-08-07 19:15:32 +0530

    class method use self, // integer division

--- linear/evendigits.py
+++ linear/evendigits.py
@@ -1,16 +1,16 @@
 #https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
 
 class Solution:
+    def evendigit(self,x):
+        n=0
+        while x>0:
+            x=x//10
+            n=n+1
+        return n%2==0
+
     def findNumbers(self, nums: List[int]) -> int:
         n=0
         for x in nums:
-            if evendigit(x):
+            if self.evendigit(x):
                 n=n+1
         return n
-
-    def evendigit(x):
-        n=0
-        while x>0:
-            x=x/10
-            n=n+1
-        return n%2==0
