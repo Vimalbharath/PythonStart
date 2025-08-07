50331c0a043d4291efe05c8fc397aa8d224fc1b7
Vimalbharath <vimalbharath21@gmail.com>
2025-08-07 20:46:22 +0530

    removed bracket high infinite

--- binary/binarysearch.py
+++ binary/binarysearch.py
@@ -1,16 +1,17 @@
 def main():
     nums=[1,2,3,4,5,6,7]
-    element=6
+    element=9
     ans=binary(nums,element)
     print(ans)
 
 def binary(nums,element):
+    if len(nums)==0: return -1
     s,e=0,len(nums)
-    while(e>s):
+    while e>s:
         m=s+(e-s)//2
-        if(nums[m]>element):
+        if nums[m]>element:
             e=m
-        elif(nums[m]<element):
+        elif nums[m]<element:
             s=m
         else:
             return m
