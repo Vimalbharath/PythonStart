55bef5b1ccaeed311ab5cca1431e971e8cf621ff
Vimalbharath <vimalbharath21@gmail.com>
2025-08-09 21:57:43 +0530

    first try last element miss

---
+++ sorting/insertion.py
@@ -0,0 +1,17 @@
+def main():
+    nums=[3,0,9,8,4,-1,-2,7,90,80,70,67,100,1]
+    insertion(nums)
+    print(nums)
+
+def insertion(nums):
+    n=len(nums)
+    for i in range(1,n):
+        for j in range(1,i):
+            if nums[j-1]>nums[j]:
+                swap(nums,j,j-1)
+
+def swap(nums,f,s):
+    nums[f],nums[s]=nums[s],nums[f]
+
+if __name__=="__main__":
+    main()
