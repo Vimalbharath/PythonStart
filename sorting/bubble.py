aeebc3b193cbb8ff02b55ed2cd0865055d708486
Vimalbharath <vimalbharath21@gmail.com>
2025-08-09 16:41:47 +0530

    bubble try

---
+++ sorting/bubble.py
@@ -0,0 +1,20 @@
+def main():
+    nums=[3,9,8,4,7]
+    bubble(nums)
+    print(nums)
+
+def bubble(nums):
+    l=len(nums)-1
+    for i in range(0,l):
+        for j in range(1,l-i):
+            if nums[j]<nums[j-1]:
+                swap(nums,j,j-1)
+
+
+def swap(nums,f,s):
+    temp=nums[f]
+    nums[f]=nums[s]
+    nums[s]=temp
+
+if __name__=="__main__":
+    main()
