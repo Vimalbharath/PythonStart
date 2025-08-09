ce6f91c1e717f3e78b9e92f6b445f5671afe654b
Vimalbharath <vimalbharath21@gmail.com>
2025-08-09 16:42:23 +0530

    break add

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -6,9 +6,13 @@ def main():
 def bubble(nums):
     l=len(nums)-1
     for i in range(0,l):
+        swapped=False
         for j in range(1,l-i):
             if nums[j]<nums[j-1]:
+                swapped=True
                 swap(nums,j,j-1)
+        if swapped==False:
+            break
 
 
 def swap(nums,f,s):
