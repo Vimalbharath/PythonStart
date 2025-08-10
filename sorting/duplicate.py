cb60912d3f9daa45fef8011ee7c11d4baca9ab36
Vimalbharath <vimalbharath21@gmail.com>
2025-08-10 13:13:50 +0530

    original cyclic sort by kunal

--- sorting/duplicate.py
+++ sorting/duplicate.py
@@ -7,9 +7,9 @@ class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         i,n=0,len(nums)
         while i<n:
-            actual=i+1
-            if nums[i]!=actual:
-                self.swap(nums,i+1,nums[i]+1)
+            actual=nums[i]
+            if nums[i]!=nums[actual]:
+                self.swap(nums,i,actual)
             else:
                 i=i+1
     
@@ -18,7 +18,7 @@ class Solution:
             
 def main():
     instance=Solution()
-    nums=[1,7,8,6,5,4,3,2]
+    nums=[1,7,8,6,5,4,3,2,0]
     print(instance.findDuplicate(nums))
     print(nums)
 
