d1cb410140ed0970b19e3dc5341a7a1f5b365bca
Vimalbharath <vimalbharath21@gmail.com>
2025-08-11 13:32:56 +0530

    local variable not accessed

--- recursion/basics/reverse.py
+++ recursion/basics/reverse.py
@@ -1,11 +1,13 @@
 def reverse(n):
-    return helper(n,1)
+    global ans=1
+    helper(n)
+    print(ans)
     
-def helper(n,ans):
+def helper(n):
     if n==0:
-        return ans
+        return
     ans=(ans*10)+n%10
-    return helper(n//10,ans)
+    return helper(n//10)
 
 def main():
     print(reverse(54321))
