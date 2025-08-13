199776c2a1c926041d0b82e38409a58b6cf364bb
Vimalbharath <vimalbharath21@gmail.com>
2025-08-13 23:10:27 +0530

    even better code

--- recursion/array/sorted.py
+++ recursion/array/sorted.py
@@ -4,9 +4,9 @@ def sorted(nums):
 def helper(nums,index):
     if index==len(nums)-1:
         return True
-    if nums[index]<=nums[index+1]:
-        return True and helper(nums,index+1)
-    return False
+    return nums[index]<=nums[index+1] and helper(nums,index+1)
+        #return True and helper(nums,index+1)
+    #return False
 
 def main():
     nums=[1,2,2,3,4,5,5,6,7,8]
