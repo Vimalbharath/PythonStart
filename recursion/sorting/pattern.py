d62846d39f9d63c3ca26c5ec0204f86b812c6b3a
Vimalbharath <vimalbharath21@gmail.com>
2025-08-14 12:10:16 +0530

    need to hard code for square

--- recursion/sorting/pattern.py
+++ recursion/sorting/pattern.py
@@ -1,13 +1,33 @@
 def pattern(r,c):
-    if c==0:
+    if r==0:
         return 
-    print("* ",end="")
-    pattern(r,c-1)
-    if c==0:
+    
+    if c<5:
+        print("* ",end="")
+        pattern(r,c+1)
+    else:
         print()
-        #pattern(r-1,c)
+        pattern(r-1,0)
+    
 
 def main():
-    pattern(5,5)
+    pattern(5,0)
 
 main()
+
+# def pattern(r,c):
+#     if r==0:
+#         return 
+    
+#     if c>0:
+#         print("* ",end="")
+#         pattern(r,c-1)
+#     else:
+#         print()
+#         pattern(r-1,5)
+    
+
+# def main():
+#     pattern(5,5)
+
+# main()
