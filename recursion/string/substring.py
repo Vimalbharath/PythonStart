a44fe785990f41588835d7f5e0fbee5781709961
Vimalbharath <vimalbharath21@gmail.com>
2025-08-17 22:13:46 +0530

    two pass ret list of p

--- recursion/string/substring.py
+++ recursion/string/substring.py
@@ -1,6 +1,6 @@
 def main():
     substring("","abc")
-    print(substringret("abc"))
+    print(substringret("","abc"))
 
 def substring(p,up):
     if len(up)==0:
@@ -10,14 +10,12 @@ def substring(p,up):
     substring(p+ch,up[1:])
     substring(p,up[1:])
 
-def substringret(up):
+def substringret(p,up):
     if len(up)==0:
-        return []
+        return [p]
     ch=up[0]
-    left=[ch]
-    temp=(substringret(up[1:]))
-    left.append(temp)
-    right=substringret(up[1:])
+    left=substringret(p+ch,up[1:])
+    right=substringret(p,up[1:])
     left.extend(right)
     return left
 
