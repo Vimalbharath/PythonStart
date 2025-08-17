d9df1d3cbbfcfed7c726471bc3fae953e1afb637
Vimalbharath <vimalbharath21@gmail.com>
2025-08-17 23:45:49 +0530

    count success

--- recursion/string/permutation.py
+++ recursion/string/permutation.py
@@ -1,15 +1,18 @@
 def main():
-    permutation("","abc")
+    print(permutation("","abc"))
     for i in range(1):
         print(i)
 
 def permutation(p,up):
     if len(up)==0:
         print(p)
-        return
+        return 1
     ch=up[0]
+    count=0
     for i in range(0,len(p)+1):
-        permutation(p[:i]+ch+p[i:],up[1:])
+        count= count+permutation(p[:i]+ch+p[i:],up[1:])
+
+    return count
         
 
 main()
