bf5a64e693deac46cd5a9def7e67e5f82b2afa81
Vimalbharath <vimalbharath21@gmail.com>
2025-08-17 20:32:46 +0530

    concatenate but performance issue

--- recursion/sorting/skip.py
+++ recursion/sorting/skip.py
@@ -1,26 +1,27 @@
 def main():
     skip("","Vimal")
-    print(skipret("Vimal"))
+    print(skipret("hgagjigjaaklgalaaaa"))
 
 def skip(p,up):
     if len(up)==0:
         print(p)
         return
-    if up[0]=='a':
+    ch=up[0]
+    if ch=='a':
         skip(p,up[1:])
     else :
-        skip(p+up[0],up[1:])
+        skip(p+ch,up[1:])
 
 def skipret(up):
     if len(up)==0:
         return ""
     ans=""
     if up[0]=='a':
-       ans.append(skipret(up[1:]))
+       ans=ans+(skipret(up[1:]))
        return ans
     else:
-        ans.append(up[0])
-        ans.append(skipret(up[1:]))
+        ans=ans+(up[0])
+        ans=ans+(skipret(up[1:]))
         return ans
 
 main()
