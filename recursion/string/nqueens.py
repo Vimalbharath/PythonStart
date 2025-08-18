ed7a36d8571f35c64bc5bfaa2f5620a354924986
Vimalbharath <vimalbharath21@gmail.com>
2025-08-18 21:08:35 +0530

    video loop- infinite

--- recursion/string/nqueens.py
+++ recursion/string/nqueens.py
@@ -1,27 +1,28 @@
+import math
 from typing import List
 
 
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         board = [[False for _ in range(n)] for _ in range(n)]
-        self.helper(board,0,0)
+        self.helper(board,0)
 
-    def helper(self,board,r,c):
+    def helper(self,board,r):
         if r==len(board):
             ans = [['Q' if val else '.' for val in row] for row in board]
             print(ans)
             return
-        if board[r][c]==False:
+        for c in range(len(board[0])):
             if not self.isSafe(board,r,c):
                 board[r][c]=True
-                self.helper(board,r+1,c)
+                self.helper(board,r+1)
                 board[r][c]=False
 
     def isSafe(self,board,r,c):
-        while r>-1:
+        for i in range(r):
             if(board[r][c]==False):
                 return False
-            r=r-1
+        #leftMin=math.min(r,c)
         return False
 
 
