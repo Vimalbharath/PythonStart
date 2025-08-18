474a8ae0c5260114df7ba288ade1fc22e8e1d3d3
Vimalbharath <vimalbharath21@gmail.com>
2025-08-18 11:50:10 +0530

    subtract before check

--- recursion/string/pathprint.py
+++ recursion/string/pathprint.py
@@ -2,11 +2,14 @@ def main():
     print(pathprint(3,3,''))
 
 def pathprint(r,c,way):
-    if r==1 or c==1:
+    if r==1 and c==1:
         print(way)
         return 1
-    left=pathprint(r-1,c,way+'D')
-    right=pathprint(r,c-1,way+'R')
+    left,right=0,0
+    if r>1:
+        left=pathprint(r-1,c,way+'D')
+    if c>1:
+        right=pathprint(r,c-1,way+'R')
     ans=left+right
     return ans
 
