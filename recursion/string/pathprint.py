70c34e35a525d33912a20b3bbb1897946759db45
Vimalbharath <vimalbharath21@gmail.com>
2025-08-18 11:52:23 +0530

    with diagnol

--- recursion/string/pathprint.py
+++ recursion/string/pathprint.py
@@ -5,12 +5,14 @@ def pathprint(r,c,way):
     if r==1 and c==1:
         print(way)
         return 1
-    left,right=0,0
+    left,right,side=0,0,0
     if r>1:
         left=pathprint(r-1,c,way+'D')
     if c>1:
         right=pathprint(r,c-1,way+'R')
-    ans=left+right
+    if r>1 and c>1:
+        side=pathprint(r-1,c-1,way+'S')
+    ans=left+right+side
     return ans
 
 main()
