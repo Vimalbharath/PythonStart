5798350765ad66f0563a83f76ecfada2113e2ecc
Vimalbharath <vimalbharath21@gmail.com>
2025-08-19 21:13:52 +0530

    delete first ok

--- linkedlist/linkedlist.py
+++ linkedlist/linkedlist.py
@@ -45,6 +45,14 @@ class LinkedList:
             node=node.next
         return node
         
+    def deletefirst(self):
+        if self.head==None:
+            print("List is Empty")
+            return
+        self.head=self.head.next
+        if self.head==None:
+            self.tail=None
+        self.size=self.size-1
         
     def display(self):
         node=self.head
@@ -56,12 +64,14 @@ class LinkedList:
 def main():
     list=LinkedList()
     list.addFirst(0)
-    list.addFirst(1)
-    list.addFirst(2)
-    list.addlast(3)
-    list.addatindex(100,1)
-    list.addatindex(200,0)
-    list.addatindex(300,6)
+    # list.addFirst(1)
+    # list.addFirst(2)
+    # list.addlast(3)
+    # list.addatindex(100,1)
+    # list.addatindex(200,0)
+    # list.addatindex(300,6)
+    list.deletefirst()
+    list.deletefirst()
     list.display()
 
 if __name__=="__main__":
