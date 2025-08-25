b0f339515e89d0259db45259d3206afdf6c0e300
Vimalbharath <vimalbharath21@gmail.com>
2025-08-25 12:43:30 +0530

    local computer crash,

--- linkedlist/reorder.py
+++ linkedlist/reorder.py
@@ -57,4 +57,40 @@ class Solution:
         # else:
         #     self.head=prev
         # newEnd.next=present
-        
+# Helper function to create a linked list from a Python list
+def create_linked_list(arr):
+    if not arr:
+        return None
+    head = ListNode(arr[0])
+    current = head
+    for val in arr[1:]:
+        current.next = ListNode(val)
+        current = current.next
+    return head
+
+# Helper function to print a linked list
+def print_linked_list(head):
+    nodes = []
+    current = head
+    while current:
+        nodes.append(str(current.val))
+        current = current.next
+    print(" -> ".join(nodes))
+
+# --- Main part to run the example ---
+
+if __name__ == '__main__':
+    # Example 1: Input: head = [1, 1, 2]
+    input_list = [1,2,3,2,1]
+    head = create_linked_list(input_list)
+
+    print("Input linked list:")
+    print_linked_list(head)
+
+    solution = Solution()
+    solution.reorderList(head)
+    
+   
+    
+    print("\nLinked list after reorder:")
+    print_linked_list(head)      
