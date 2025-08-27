ba49bc08067c279cc861b7acab6e763ca9a197ee
Vimalbharath <vimalbharath21@gmail.com>
2025-08-27 09:39:49 +0530

    ,python remove[0],

---
+++ stacks/parenthesis.py
@@ -0,0 +1,19 @@
+#https://leetcode.com/problems/valid-parentheses/
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        for char in s:
+            if char=='[' or char =='{' or char =='(':
+                stack.append(char)
+            else:
+                if char==']':
+                    if len(stack)==0 or not stack.remove(stack[0])=='[':
+                        return False
+                if char=='}':
+                    if len(stack)==0 or not stack.remove(stack[0])=='{':
+                        return False
+                if char==')':
+                    if len(stack)==0 or not stack.remove(stack[0])=='(':
+                        return False
+        return len(stack)==0
