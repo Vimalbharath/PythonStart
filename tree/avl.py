742105d0afa66513a4e9bac360b3ab81bf14adb0
Vimalbharath <vimalbharath21@gmail.com>
2025-08-27 21:49:35 +0530

    height fun short,rotate,balanced??video

---
+++ tree/avl.py
@@ -0,0 +1,69 @@
+class AVL:
+    class Node:
+        def __init__(self,val):
+            self.val=val
+            self.left=None
+            self.right=None
+            self.height=0
+        
+    def __init__(self):
+        self.head=None
+
+    def insert(self,val):
+        self.head=self.inserthelper(val,self.head)
+
+    def inserthelper(self,val,node):     
+        if not node:
+            newnode=self.Node(val)
+            node=newnode
+            return node
+        if val<node.val:
+            node.left=self.inserthelper(val,node.left)
+        else:
+            node.right=self.inserthelper(val,node.right)
+        node.height=max(self.height(node.left),self.height(node.right))+1
+        return self.rotate(node)
+    
+    def height(self,node):
+        if node:
+            return node.height
+        else:
+            return 0
+    
+    def rotate(self,node):
+        if node.height<=1:
+            return node
+        print ("Unbalanced",node.height,node.val)
+        return node
+    
+    def balanced(self):
+        return self.bal(self.head)
+    def bal(self,node):
+        if not node:
+            return
+        return node.height<=1 and self.bal(node.left) and self.bal(node.right)
+
+    def preorder(self,node):
+        if not node:
+            return
+        self.preorder(node.left)
+        print(node.val,node.height)
+        self.preorder(node.right)
+
+if __name__=="__main__":
+    tree=AVL()
+    tree.insert(5)
+    tree.insert(3)
+    tree.insert(2)
+    # tree.insert(4)
+    # tree.insert(1)
+    # tree.insert(7)
+    # tree.insert(9)
+    # tree.insert(0)
+    tree.preorder(tree.head)
+    print(tree.balanced())
+        
+        
+
+
+
