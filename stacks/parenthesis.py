1254fdb7d4290db9b801d18c6ce5eb1999517cd4
Vimalbharath <vimalbharath21@gmail.com>
2025-08-27 09:41:22 +0530

    stack[len(stack)-1]

--- stacks/parenthesis.py
+++ stacks/parenthesis.py
@@ -8,12 +8,12 @@ class Solution:
                 stack.append(char)
             else:
                 if char==']':
-                    if len(stack)==0 or not stack.remove(stack[0])=='[':
+                    if len(stack)==0 or not stack.remove(stack[len(stack)-1])=='[':
                         return False
                 if char=='}':
-                    if len(stack)==0 or not stack.remove(stack[0])=='{':
+                    if len(stack)==0 or not stack.remove(stack[len(stack)-1])=='{':
                         return False
                 if char==')':
-                    if len(stack)==0 or not stack.remove(stack[0])=='(':
+                    if len(stack)==0 or not stack.remove(stack[len(stack)-1])=='(':
                         return False
         return len(stack)==0
