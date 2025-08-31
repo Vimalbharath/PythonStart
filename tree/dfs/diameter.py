477ac5fc4d7f60db12071e54ae603c5da81636cc
Vimalbharath <vimalbharath21@gmail.com>
2025-08-31 14:30:06 +0530

    using constructor

--- tree/dfs/diameter.py
+++ tree/dfs/diameter.py
@@ -10,12 +10,18 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
+    def __init__(self):
+        # Initialize an instance variable to store the diameter
+        self.diameter = 0
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        return max(self.height(root),self.height(root.left),self.height(root.right))-1
+        self.height(root)
+        return self.diameter-1
 
     def height(self,node):
         if node==None:
             return 0
         left=self.height(node.left)
         right=self.height(node.right)
+        dia=right+left+1
+        self.diameter=max(dia,self.diameter)
         return max(right,left)+1
