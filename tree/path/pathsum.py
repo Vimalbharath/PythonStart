827c554a1b6519ae308d98c56f55422f480561a6
Vimalbharath <vimalbharath21@gmail.com>
2025-09-01 12:30:02 +0530

    path,partial work,

---
+++ tree/path/pathsum.py
@@ -0,0 +1,15 @@
+#https://leetcode.com/problems/path-sum/description/
+# 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        left=self.hasPathSum(root.left,targetSum-root.val)
+        right=self.hasPathSum(root.right,targetSum-root.val)
+        return left or right
