e57111c5032680b902b345cab2a77e07892f4e91
Vimalbharath <vimalbharath21@gmail.com>
2025-09-05 14:07:55 +0530

    error need to fix,

--- graph/graphlist.py
+++ graph/graphlist.py
@@ -3,16 +3,28 @@ class Vertex:
         self.name=name
         self.edges=[]
 
+    def __str__(self):
+        ans=''
+        ans=ans+ "a"+self.name
+        return ans
+    
     def getEdges(self):
         return self.edges
     
     def addEdge(self,v2,w=0):
-        self.edges.append(v2,w)
+        self.edges.append(v2)
     
-class Edge:
-    def __init__(self,v1,v2,w=0):
-        v1.addEdge(v2,w)
+# class Edge:
+#     def __init__(self,v1,v2,w=0):
+#         self.v1=v1
+#         self.v2=v2
+#         self.w=w
 
+#     def __str__(self):
+#         ans=''
+#         ans=ans+ self.v1+self.v2+self.w
+#         return ans
+    
 class GraphList:
     def __init__(self,directed,weighted):
         self.vertices=[]
@@ -20,7 +32,10 @@ class GraphList:
         self.weighted=weighted
 
     def __str__(self):
-        return self.vertices
+        ans=''
+        for i in self.vertices:
+            ans=ans+ "---"+i.name
+        return ans
     
     def addVertex(self,name):
         newVertex=Vertex(name)
@@ -31,11 +46,11 @@ class GraphList:
         return self.vertices
     
     def addEdge(self,v1,v2,w=0):
-        Edge(v1,v2,w)
+        v1.addEdge(v2,w)
         if not self.directed:
-            Edge(v2,v1,w)
+            v2.addEdge(v1,w)
 
-if __name__=="__main__()":
+if __name__=="__main__":
     graph=GraphList(True,True)
     ch=graph.addVertex("Chennai")
     dl=graph.addVertex("Delhi")
@@ -43,3 +58,4 @@ if __name__=="__main__()":
     graph.addEdge(ch,dl,800)
     graph.addEdge(ch,mb,400)
     print(graph)
+    print(ch.getEdges())
