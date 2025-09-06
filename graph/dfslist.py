6e1e437f4fedb98b63855d4e15a07ed37596c5bf
Vimalbharath <vimalbharath21@gmail.com>
2025-09-06 12:16:04 +0530

    dfs stack try break visited-fix,

---
+++ graph/dfslist.py
@@ -0,0 +1,41 @@
+from graphlist import GraphList
+from collections import deque
+
+class GraphListBFS(GraphList):
+    def dfs(self):
+       ans=[]
+       vertices=self.getVertices()
+       visited=[False]*len(vertices)
+       stack=deque([vertices[0]])
+       visited[0]=True
+       while stack:
+            current=stack[-1]
+            edges=current.getEdges2()
+            for e in edges:
+                endVertex=e.v2
+                pos=vertices.index(endVertex)
+                if not visited[pos]:
+                    visited[pos]=True
+                    stack.append(endVertex)
+                    break
+            ans.append(stack.pop().name)
+       ans.reverse()
+       print(ans)
+
+
+
+if __name__=="__main__":
+    graph=GraphListBFS(False,True)
+    ch=graph.addVertex("Chennai")
+    dl=graph.addVertex("Delhi")
+    
+    sl=graph.addVertex("SriLanka")
+    mb=graph.addVertex("Mumbai")
+    bg=graph.addVertex("Bangalore")
+    kl=graph.addVertex("Kolkata")
+    graph.addEdge(ch,dl,800)
+    graph.addEdge(ch,mb,800)
+    graph.addEdge(mb,bg,300)
+    graph.addEdge(bg,kl,400)
+    print(graph)
+    graph.dfs()
