654d7b7c705672ea0b47927dad21b62a02be054f
Vimalbharath <vimalbharath21@gmail.com>
2025-09-07 14:56:32 +0530

    find return miss

--- graph/unionfind.py
+++ graph/unionfind.py
@@ -7,7 +7,7 @@ class DSU:
 
     def find(self,x):
         if not self.parent[x]==x:
-            self.find(self.parent[x])
+            return self.find(self.parent[x])
         return self.parent[x]
     
     def union(self,x,y):
