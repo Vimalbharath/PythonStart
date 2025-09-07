71f4cbddb8810d971defcc72bb465fa692dca6cf
Vimalbharath <vimalbharath21@gmail.com>
2025-09-07 14:46:57 +0530

    working inefficient detect cycle,

--- Readme.md
+++ Readme.md
@@ -93,4 +93,4 @@ September-5-2025 : graph matrix try,graphlist try empty print,error need to fix,
 September-6-2025 : graph list working,module init file no use,bfs stage,halfway in bfs,BFS working fine,
 dfs stack try break visited-fix,for else works-dfs stack,corrected dfs approach,dfs using rec,used set instead of index,topo outdegree,indegree,efficient using dict,topo try with dfs cycle??,sorting but can't detect cycle,topo khan - fail,not used queue, topodfs cycle extra rec st set skip.//Kruskal- all edges double,unique edges gemini,w3schools similar,room key fail
 
-September-7-2025 : Union Find- no idea- parent array,tried after video,
+September-7-2025 : Union Find- no idea- parent array,tried after video,working inefficient detect cycle,
--- graph/unionfind.py
+++ graph/unionfind.py
@@ -1,11 +1,13 @@
 class DSU:
-    def __init__(self):
-        self.parent=[]
+    def __init__(self,n):
+        self.parent=[0]*n
 
     def make_set(self,x):
         self.parent[x]=x
 
     def find(self,x):
+        if not self.parent[x]==x:
+            self.find(self.parent[x])
         return self.parent[x]
     
     def union(self,x,y):
@@ -13,4 +15,21 @@ class DSU:
         yy=self.find(y)
         if xx==yy:
             return
-        self.parent[x]=y
+        self.parent[xx]=yy
+
+if __name__=="__main__":
+    dsu=DSU(6)
+    for i in range(6):
+        dsu.make_set(i)
+    edges=[[0,1],[0,2],[1,3],[3,4],[4,5]]
+    for edge in edges:
+        x=dsu.find(edge[0])
+        y=dsu.find(edge[1])
+        if x==y:
+            print("Cycle Present")
+            break
+        else:
+            dsu.union(edge[0],edge[1])
+    else:
+        print("No Cycle")
+    
