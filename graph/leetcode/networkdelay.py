e5efe01b51c8f39b4d59ea65a167c51bd73d2f88
Vimalbharath <vimalbharath21@gmail.com>
2025-09-15 20:45:41 +0530

    dijstra confuse using 1-n

---
+++ graph/leetcode/networkdelay.py
@@ -0,0 +1,33 @@
+#https://leetcode.com/problems/network-delay-time/
+
+import math
+import heapq
+from typing import List
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adj=[[]*n for _ in range(n)]
+        for edge in times:
+            adj[edge[0]].append((edge[1],edge[2]))
+        key=[math.inf]*n
+        key[k-1]=0
+        minheap=[(key[k-1],k)]
+        print(adj)
+        while minheap:
+            currdist,u=heapq.heappop(minheap)
+            for adjedge in adj[u] :
+                v=adjedge[0]
+                l=adjedge[1]
+                if key[v-1]>currdist+l:
+                    key[v-1]=min(currdist+l,key[v-1])
+                    heapq.heappush(minheap,(key[v-1],v-1))
+        print(key)
+
+
+
+if __name__=="__main__":
+    sol=Solution()
+    times = [[2,1,1],[2,3,1],[3,4,1]]
+    n = 4
+    k = 2
+    sol.networkDelayTime(times,n,k)
