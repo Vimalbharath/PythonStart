f7d5bfd865331ead195dfa5c00099accccf872c8
Vimalbharath <vimalbharath21@gmail.com>
2025-09-18 21:33:16 +0530

    tabulation but not rec relation,

---
+++ dp/table.py/fibo.py
@@ -0,0 +1,12 @@
+def fibo(n):
+    table=[0]*(n+1)
+    table[1]=1
+    for i in range(n):
+        if i+1<=n:
+            table[i+1]+=table[i]
+        if i+2<=n:
+            table[i+2]+=table[i]
+    return table[n]
+
+if __name__=="__main__":
+    print(fibo(50))
