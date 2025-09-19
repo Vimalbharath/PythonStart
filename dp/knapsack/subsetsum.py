e31660319aa6cb86fda8f10a9fb827397d608ebb
Vimalbharath <vimalbharath21@gmail.com>
2025-09-19 21:53:03 +0530

    debuged table[i-1][j-arr[i-1]],

--- dp/knapsack/subsetsum.py
+++ dp/knapsack/subsetsum.py
@@ -7,7 +7,7 @@ def subsetsum(arr,sum):
     for i in range(1,len(arr)+1):
         for j in range(1,sum+1):
             if arr[i-1]<=j:
-                table[i][j]=table[i-1][j-1] or table[i-1][j]
+                table[i][j]=table[i-1][j-arr[i-1]] or table[i-1][j]
             else:
                 table[i][j]=table[i-1][j]
     return table
