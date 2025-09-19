4788b799b78b04df40f9214eb85fc9528de658f0
Vimalbharath <vimalbharath21@gmail.com>
2025-09-19 20:18:43 +0530

    added memo,

--- dp/knapsack/zeroone.py
+++ dp/knapsack/zeroone.py
@@ -1,10 +1,14 @@
-def zeroone(val,w,weight,n):
+def zeroone(val,w,weight,n,memo={}):
     if not weight or not n:
         return 0
+    if (weight,n) in memo:
+        return  memo[(weight,n)]
     if w[n-1]<=weight:
-        return max(val[n-1]+zeroone(val,w,weight-w[n-1],n-1),zeroone(val,w,weight,n-1))
+        memo[(weight,n)]=max(val[n-1]+zeroone(val,w,weight-w[n-1],n-1),zeroone(val,w,weight,n-1))
+        return memo[(weight,n)]
     else:
-        return zeroone(val,w,weight,n-1)
+        memo[(weight,n)]=zeroone(val,w,weight,n-1)
+        return memo[(weight,n)]
 
 if __name__=="__main__":
     weight=14
