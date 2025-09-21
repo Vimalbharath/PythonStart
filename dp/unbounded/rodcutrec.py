e990a4cfe336bfa2b2ce105caaa9ea191782a0b5
Vimalbharath <vimalbharath21@gmail.com>
2025-09-21 13:19:18 +0530

    how sum ok,

--- dp/unbounded/rodcutrec.py
+++ dp/unbounded/rodcutrec.py
@@ -1,14 +1,16 @@
-def rod(price,n):
+def rod(price,n,ans):
     if n==0:
-        return True
+        return ans
     if n<0:
-        return False
+        return -1
     for i in price:
         newlength=n-i
-        if rod(price,newlength)==True:
-            return True
-    return False
+        if newlength<=n:
+            ans.append(i)
+            if not rod(price,newlength,ans)==-1:
+                return ans
+    return -1
 
 if __name__=="__main__":
     price=[3, 5]
-    print(rod(price,9))
+    print(rod(price,9,[]))
