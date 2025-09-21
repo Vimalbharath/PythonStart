cf2fb85740237af85841e7de1dcca8bbe0d27575
Vimalbharath <vimalbharath21@gmail.com>
2025-09-21 18:51:59 +0530

    video copy best,

--- dp/unbounded/bestsum.py
+++ dp/unbounded/bestsum.py
@@ -3,19 +3,17 @@ def rod(price,n):
         return []
     if n<0:
         return None
+    bestresult=None
     for i in price:
         newlength=n-i
         result=rod(price,newlength)
-        bestresult=None
         if not result==None:
             result.append(i)
-            if not bestresult:
-                bestresult=result
-            if len(result)<=len(bestresult):
+            if not bestresult or len(result)<=len(bestresult):
                 bestresult=result
     return bestresult
     
 
 if __name__=="__main__":
-    price=[3,5,4,7]
+    price=[4,3,5]
     print(rod(price,7))
