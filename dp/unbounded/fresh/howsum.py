4b539a6f669ec46f6769f8b31cf34dc85175aae8
Vimalbharath <vimalbharath21@gmail.com>
2025-09-22 15:36:48 +0530

    how rec ok

--- dp/unbounded/fresh/howsum.py
+++ dp/unbounded/fresh/howsum.py
@@ -3,24 +3,14 @@ def howsum(arr,target,n):
         return []
     if n==0 or target<0:
         return None
-    ans=None
-    if arr[n-1]<=target:
-        include=howsum(arr,target-arr[n-1],n) 
-        exclude=howsum(arr,target,n-1)
-        if not include==None:
-            include=include+[arr[n-1]]
-        else:
-            include=[arr[n-1]]
-        if exclude:
-            ans=include+exclude
-        else:
-            ans=include
-    else:
-        result=howsum(arr,target,n-1)
-        if not result==None:
-            ans=result
-    return ans
+    include=howsum(arr,target-arr[n-1],n) 
+    if not include ==None:
+        return include+[arr[n-1]]
+    exclude=howsum(arr,target,n-1)
+    if not exclude ==None:
+        return exclude
+    return None
 
 if __name__=="__main__":
-    arr=[2,5,6]
+    arr=[2,5]
     print(howsum(arr,11,len(arr)))
