9368cacde5201e3c917251f62c38b2f31bb49faf
Vimalbharath <vimalbharath21@gmail.com>
2025-09-24 20:24:30 +0530

    rec corrected index,

--- dp/lcs/rec.py
+++ dp/lcs/rec.py
@@ -1,10 +1,10 @@
 def lcs(a,b):
-    return lcshelper(a,b,len(a)-1,len(b)-1)
+    return lcshelper(a,b,len(a),len(b))
 
 def lcshelper(a,b,m,n):
     if m==0 or n==0:
         return 0
-    if a[m]==b[m]:
+    if a[m-1]==b[n-1]:
         return 1+lcshelper(a,b,m-1,n-1)
     return max(lcshelper(a,b,m,n-1),lcshelper(a,b,m-1,n))
 
