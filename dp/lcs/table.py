4b1e1b4c61442847fcd4b03bad2b886c5c27202a
Vimalbharath <vimalbharath21@gmail.com>
2025-09-24 20:44:22 +0530

    table lite bug corrected,

---
+++ dp/lcs/table.py
@@ -0,0 +1,21 @@
+def lcs(a,b):
+    m,n=len(a),len(b)
+    table=[[-1] * (n+1) for _ in range(m+1)] 
+    for j in range(n+1):
+        table[0][j]=0
+    for i in range(m+1):
+        table[i][0]=0
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            if a[i-1]==b[j-1]:
+                table[i][j]=1+table[i-1][j-1]
+            else:
+                table[i][j]=max(table[i-1][j],table[i][j-1])
+    print(table)
+    return table[m][n]
+
+if __name__=="__main__":
+    a="abcdef"
+    b="abrfd"
+    print(lcs(a,b))
+
