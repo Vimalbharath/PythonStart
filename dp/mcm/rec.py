2d7c3fff5c34dbc3c71f3ab5be95300b94023d70
Vimalbharath <vimalbharath21@gmail.com>
2025-09-25 16:06:00 +0530

    added memo mcm,

--- dp/mcm/rec.py
+++ dp/mcm/rec.py
@@ -1,15 +1,18 @@
 import math
 
 def mcm(arr):
-    return mcmhelp(arr,1,len(arr)-1)
+    return mcmhelp(arr,1,len(arr)-1,memo={})
 
-def mcmhelp(arr,i,j):
+def mcmhelp(arr,i,j,memo):
     if i>=j:
         return 0
+    if (i,j) in memo:
+        return memo[(i,j)]
     ans=math.inf
     for k in range(i,j):
-        temp=mcmhelp(arr,i,k)+mcmhelp(arr,k+1,j)+(arr[i-1]*arr[k]*arr[j])
+        temp=mcmhelp(arr,i,k,memo)+mcmhelp(arr,k+1,j,memo)+(arr[i-1]*arr[k]*arr[j])
         ans=min(temp,ans)
+    memo[(i,j)] =ans
     return ans
 
 if __name__=="__main__":
