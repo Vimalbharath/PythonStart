a9ebe147a27b63d792ffde5e96b670c1f4918f41
Vimalbharath <vimalbharath21@gmail.com>
2025-09-25 21:24:25 +0530

    added memo,

--- dp/mcm/palindromepart.py
+++ dp/mcm/palindromepart.py
@@ -1,17 +1,31 @@
 import math
 
 def partpalin(s):
-    return partpalinh(s,0,len(s)-1)
+    return partpalinh(s,0,len(s)-1,memo={})
 
-def partpalinh(s,i,j):
+def partpalinh(s,i,j,memo):
     if i>=j:
         return 0
+    if (i,j) in memo:
+        return memo[(i,j)]
     if palindrome(s,i,j):
         return 0
     ans=math.inf
     for k in range(i,j):
-        temp=1+partpalinh(s,i,k)+partpalinh(s,k+1,j)
+        # if (i,k) in memo:
+        #     left=memo[(i,k)]
+        # else:
+        #     memo[(i,k)]=partpalinh(s,i,k,memo)
+        #     left=memo[(i,k)]
+        # if (k+1,j) in memo:
+        #     right=memo[(k+1,j)]
+        # else:
+        #     memo[(k+1,j)]=partpalinh(s,k+1,j,memo)
+        #     right=memo[(k+1,j)]
+        # temp=1+left+right
+        temp=1+partpalinh(s,i,k,memo)+partpalinh(s,k+1,j,memo)
         ans=min(temp,ans)
+    memo[(i,j)]=ans
     return ans
 
 def palindrome(s,i,j):
