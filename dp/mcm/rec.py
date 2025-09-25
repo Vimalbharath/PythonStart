853ed2a3ae3b5d267b536a48b7824e7ffa15f895
Vimalbharath <vimalbharath21@gmail.com>
2025-09-25 15:52:21 +0530

    rec try inf,

---
+++ dp/mcm/rec.py
@@ -0,0 +1,17 @@
+import math
+
+def mcm(arr):
+    return mcmhelp(arr,1,len(arr)-1)
+
+def mcmhelp(arr,i,j):
+    if i>=j:
+        return 0
+    ans=math.inf
+    for k in range(i,j-1):
+        temp=mcmhelp(arr,i,k)+mcmhelp(arr,k+1,j)+(arr[i-1]*arr[k]*arr[k+1])
+        ans=min(temp,ans)
+    return ans
+
+if __name__=="__main__":
+    arr=[2,1,3,4]
+    print(mcm(arr))
