51317d61bbceedf485188d701e0daf40fb8d61a2
Vimalbharath <vimalbharath21@gmail.com>
2025-09-27 22:16:45 +0530

    totally confused,

--- slidingwindow/maxsum.py
+++ slidingwindow/maxsum.py
@@ -9,22 +9,25 @@ def maxsum(arr,k):
     return ans
 
 def maxsumslide(arr,k):
-    i,j=0,len(arr)-1
-    while(not i==j):
-        window=0
-        temp=0
-        ans=0
-        if not window==k:
-            i=i+1
-            temp=temp+arr[i]
-            window=window+1
-        else:
+    i,j=0,0
+    temp=0
+    ans=0
+    while(not j==len(arr)-1):
+        window=j-i
+        
+        if window==k:
             ans=max(temp,ans)
-            temp=temp-
+            temp=temp-arr[i]
+            i=i+1
+        temp=temp+arr[j]
+        j=j+1
+            
+        
+    return ans
 
 
 
 if __name__=="__main__":
-    arr=[1,2,3,4,5,6,7,8,9]
-    print(maxsum(arr,3))
-    print(maxsumslide(arr,3))
+    arr=[1,2,3,4]
+    print(maxsum(arr,2))
+    print(maxsumslide(arr,2))
