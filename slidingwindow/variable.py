bf3d4cedc4b8d7c02b6b440f03b5c8831e187d6c
Vimalbharath <vimalbharath21@gmail.com>
2025-09-28 16:02:31 +0530

    added min,

--- slidingwindow/variable.py
+++ slidingwindow/variable.py
@@ -1,13 +1,15 @@
+import math
+
 def minsubarr(arr,sum):  
-    ans=0
+    ans=math.inf
     for i in range(len(arr)):
         wsum=0
         for j in range(i,len(arr)):
             wsum+=arr[j]
             if wsum==sum:
-                ans=max(ans,j-i+1)
+                ans=min(ans,j-i+1)
     return ans
 
 if __name__=="__main__":
-    arr=[1,2,3,4,1,2,1]
-    print(minsubarr(arr,10))
+    arr=[1,2,3,2,1]
+    print(minsubarr(arr,7))
