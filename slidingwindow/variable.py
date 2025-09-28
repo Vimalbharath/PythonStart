b1f97d52856629b898b12cc0a4fba0b69d939471
Vimalbharath <vimalbharath21@gmail.com>
2025-09-28 16:23:09 +0530

    variable slide try,

--- slidingwindow/variable.py
+++ slidingwindow/variable.py
@@ -10,6 +10,24 @@ def minsubarr(arr,sum):
                 ans=min(ans,j-i+1)
     return ans
 
+def minsubarrslide(arr,sum):
+    ans=math.inf
+    i,j=0,0
+    wsum=0
+    while j<len(arr) :
+        wsum+=arr[j]
+        if wsum<sum:
+            j=j+1
+        if wsum==sum:
+            ans=min(ans,j-i+1)
+        if wsum>sum:
+            if i<len(arr):
+                wsum-=arr[i]
+                i=i+1
+                j=j+1
+    return ans
+
 if __name__=="__main__":
     arr=[1,2,3,2,1]
     print(minsubarr(arr,7))
+    print(minsubarrslide(arr,7))
