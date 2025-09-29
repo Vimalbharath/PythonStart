b097a766c82c8a814c842af4e38e1f910d2a136e
Vimalbharath <vimalbharath21@gmail.com>
2025-09-29 11:29:39 +0530

    high alt range mistake,

---
+++ prefixsum/highalt.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/find-the-highest-altitude/?envType=problem-list-v2&envId=prefix-sum
+
+
+from typing import List
+
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        prefix=[0 for _ in range(len(gain))]
+        prefix[0]=0
+        for i in range(1,len(gain)-1):
+            prefix[i]=prefix[i-1]+gain[i-1]
+        print(prefix)
+        return max(prefix)
+    
+if __name__=="__main__":
+    sol=Solution()
+    gain = [-5,1,5,0,-7]
+    sol.largestAltitude(gain)
