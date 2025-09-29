4606beb358c4e8eb35b5a52cd4c0d58868288565
Vimalbharath <vimalbharath21@gmail.com>
2025-09-29 11:30:21 +0530

    corrected

--- prefixsum/highalt.py
+++ prefixsum/highalt.py
@@ -6,9 +6,9 @@ from typing import List
 
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        prefix=[0 for _ in range(len(gain))]
+        prefix=[0 for _ in range(len(gain)+1)]
         prefix[0]=0
-        for i in range(1,len(gain)-1):
+        for i in range(1,len(gain)+1):
             prefix[i]=prefix[i-1]+gain[i-1]
         print(prefix)
         return max(prefix)
