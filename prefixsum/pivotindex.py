621d690bbf0de0b96ee33cf9d1c447334994f910
Vimalbharath <vimalbharath21@gmail.com>
2025-09-29 12:23:46 +0530

    pivot try,

---
+++ prefixsum/pivotindex.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/find-pivot-index/?envType=problem-list-v2&envId=prefix-sum
+
+from typing import List
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        sumLeft=[0 for i in range(len(nums))]
+        sumRight=[0 for i in range(len(nums))]
+        sumLeft[0]=nums[0]
+        sumRight[0]=nums[len(nums)-1]
+        for i in range(1,len(nums)-1):
+            sumLeft[i]=sumLeft[i-1]+nums[i]
+            sumRight[len(nums)-1-i]=sumRight[len(nums)-i]+nums[len(nums)-1-i]
+        print(sumLeft)
+        print(sumRight)
+        for i in range(len(nums)):
+            if sumLeft[i]==sumRight[i]:
+                return i
+        return -1
+    
+if __name__=="__main__":
+    sol=Solution()
+    nums = [1,7,3,6,5,6]
+    print(sol.pivotIndex(nums))
