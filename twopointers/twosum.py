706a831941d0ece209b850d75fa03dcd5a94df11
Vimalbharath <vimalbharath21@gmail.com>
2025-09-30 15:29:41 +0530

    n log(n) perfect,

--- twopointers/twosum.py
+++ twopointers/twosum.py
@@ -1,5 +1,5 @@
 def twosum(arr,target):
-    arr.sort()
+    #arr.sort()
     print(arr)
     for i in range(len(arr)):
         sum=arr[i]
@@ -7,13 +7,32 @@ def twosum(arr,target):
             if sum+arr[j]==target:
                 return True
     return False
+
+def twosum2(arr,target):
+    arr.sort()
+    i=0
+    j=len(arr)-1
+    sum=0
+    while i<j:
+        sum=arr[i]+arr[j]
+        if sum==target:
+            return True
+        if sum>target:
+            j=j-1
+        else:
+            i=i+1
     
 if __name__=="__main__":
     arr = [0, -1, 2, -3, 1]
-    target = -5
+    target = 4
 
     # Call the two_sum function and print the result
     if twosum(arr, target):
+        print("true")
+    else:
+        print("false")
+
+    if twosum2(arr, target):
         print("true")
     else:
         print("false")
